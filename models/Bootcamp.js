ffb7e8867bb8c265f8d434e4c17c3695c8af945b
sarang vasishth <sarang.vasishth@gmail.com>
2020-04-06 10:44:34 +0530

    before bootcamp CRUD APIs

---
+++ models/Bootcamp.js
@@ -0,0 +1,96 @@
+const moongoose = require('mongoose');
+const BootcampSchema = new moongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please add a name'],
+		unique: true,
+		trim: true,
+		maxlength: [50, 'Name cannot exceed more than 50 characters'],
+	},
+	slug: String,
+	description: {
+		type: String,
+		required: [true, 'Please add a description'],
+		maxlength: [500, 'Description cannot exceed more than 500 characters'],
+	},
+	website: {
+		type: String,
+	},
+	phone: {
+		type: String,
+		maxlength: [20, 'Phone number can not be longer than 20 characters'],
+	},
+	email: {
+		type: String,
+	},
+	address: {
+		type: String,
+		maxlength: [true, 'Please add an address'],
+	},
+	location: {
+		type: {
+			type: String, // Don't do `{ location: { type: String } }`
+			enum: ['Point'], // 'location.type' must be 'Point'
+			required: true,
+		},
+		coordinates: {
+			type: [Number],
+			required: true,
+			index: '2dsphere',
+		},
+		formattedAddress: String,
+		street: String,
+		city: String,
+		state: String,
+		zipCode: String,
+		country: String,
+	},
+	careers: {
+		type: [String],
+		required: true,
+		enum: ['Web Development', 'Mobile Development', 'UI/UX', 'Data Science', 'Business', 'Other'],
+	},
+	averageRating: {
+		type: Number,
+		min: [1, 'Rating must be atleast 1'],
+		max: [10, 'Rating must be not exceed 10'],
+	},
+	averageCost: Number,
+	photo: {
+		type: String,
+		default: 'no-photo.jpg',
+	},
+
+	housing: {
+		type: Boolean,
+		default: false,
+	},
+	jobAssistance: {
+		type: Boolean,
+		default: false,
+	},
+	jobGuarantee: {
+		type: Boolean,
+		default: false,
+	},
+	acceptGi: {
+		type: Boolean,
+		default: false,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+BootcampSchema.path('website').validate((val) => {
+	const urlRegex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+	return urlRegex.test(val);
+}, 'Please use a valid URL with HTTP or HTTPS');
+
+BootcampSchema.path('email').validate((val) => {
+	const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return emailRegex.test(String(val).toLowerCase());
+}, 'Please enter a valid Email');
+
+module.exports = moongoose.model('Bootcamp', BootcampSchema);
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const dotenv = require('dotenv');
-const express = require('express');
 const morgan = require('morgan');
-
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const express = require('express');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+connectDB();
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -16,6 +18,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`);
+const server = app.listen(PORT, () => {
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow.bold);
+});
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	server.close(() => process.exit(1));
 });
